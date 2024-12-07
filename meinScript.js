dace10571fd9c6647654c7b778411fc4e254b6d0
Volkan Akkan <akkan.volkan@gmail.com>
2024-12-07 14:22:05 +0100

    update

--- meinScript.js
+++ meinScript.js
@@ -2,5 +2,15 @@
 
 let name = prompt("Wie heißen Sie?");
 let age = prompt("Wie alt sind Sie?");
+let town = "Nettetal";
+let num = 7;
 
+console.log(typeof num);
+console.log(typeof town);
 alert("Hallo " + name + "(" + age + ")," + " ich heiße Sie herzlich Willkommen!");
+
+if(num == 7) {
+    document.write("richtig")
+} else {
+    document.write("wrong")
+}
